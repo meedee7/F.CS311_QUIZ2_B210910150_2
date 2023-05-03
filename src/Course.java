cb2a6c04eb0832591cebd1c9e2c00038a7517340
meedee7 <118151862+meedee7@users.noreply.github.com>
2023-05-03 14:44:00 +0800

    cfxz

---
+++ src/Course.java
@@ -0,0 +1,15 @@
+public class Course {
+
+	public String courseName;
+	Course (String courseName){
+		this.courseName = courseName;
+		
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+}
